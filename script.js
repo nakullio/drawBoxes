070bda1e381d1704f744046c1ddfd12ca6f93fb9
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-01-11 23:14:27 +0700

    get body structure from js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-drawBoxes();
+window.onload = function () {
+  drawBoxes();
+};
 
 function drawBoxes() {
   var html = `<div class="red-box"></div>`;
-  var body = document.getElementsByTagName("body");
-  console.log(body);
+  var body = document.getElementsByTagName("body")[0];
+  body.innerHTML = html;
 }
